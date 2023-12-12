edc211904c73898888250e3785543b77be191e31
DanieLuan <danielluanlourencol@gmail.com>
2023-12-12 14:21:28 -0300

    fix: responsive create project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 import "./Header.css"
 
-import logo from "../../img/logo-name.png";
+import logo_name from "../../img/logo-name.png";
+import logo from "../../img/logo.png";
 
 export default function Header() {
   return (
     <header id="header-main-page">
+      <div id='header-logo-images'>
         <img src={logo} alt="logo" id="header-logo" />
+        <img src={logo_name} alt="logo" id="header-logo-name" />
+      </div>
         <p>Educação para <strong>todos</strong>, com a ajuda de <strong>todos</strong>.</p>
     </header>
   )
